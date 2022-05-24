4b449f7d56d34eac56c89082bdaa9b2fe78b7ba0
Gj <gongjie0422@163.com>
2022-05-24 16:42:24 +0800

    fix: modify

--- packages/shell-chrome/src/components/test.tsx
+++ packages/shell-chrome/src/components/test.tsx
@@ -1,7 +1,22 @@
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
+import { NButton } from "naive-ui";
 
-export default defineComponent({
-    render() {
-        return <div>hahaha ddd</div>
-    }
+export default defineComponent(() => {
+    onMounted(() => {
+        console.log(123)
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://fakeapi.jsonparseronline.com/users/1', true);
+        xhr.send();
+        xhr.onload = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(this.responseText, 'xhr');
+            }
+        }
+        fetch("http://fakeapi.jsonparseronline.com/users/1")
+            .then(response => response.text())
+            .then(json => console.log(json, 'fetch'))
+    })
+    return <div>
+        <NButton>Hello</NButton>
+    </div>
 })
--- packages/shell-chrome/src/main.ts
+++ packages/shell-chrome/src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './reset.css'
+import lib from "colibri-lib";
 
+lib.update({
+    switch_on: true,
+    matching_content: [
+        {
+            switch_on: true,
+            match_url: 'http://fakeapi.jsonparseronline.com/users/1',
+            override: JSON.stringify({ ok: true, text: "拦截成功" }),
+        }
+    ]
+})
 createApp(App).mount('#app')
