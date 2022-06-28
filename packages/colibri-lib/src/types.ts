a77ef3de1e0c526ef915a986412cd97ea311200e
Gj <gongjie0422@163.com>
2022-06-28 20:38:17 +0800

    fix: modify

--- packages/colibri-lib/src/types.ts
+++ packages/colibri-lib/src/types.ts
@@ -1,12 +1,20 @@
 export type IFilterType = "normal" | "regex";
 export interface IMatchContent {
+    /**是否需要匹配 */
     switch_on: boolean;
+    /**匹配目标URL */
     match_url: string;
+    /**需要覆盖的内容 */
     override?: string;
+    /**匹配规则 */
     filter_type?: IFilterType;
+    /**请求协议 */
+    method?: string;
 }
 
 export interface IGlobalState {
+    /**全局开关 */
     switch_on: boolean;
+    /**规则列表 */
     matching_content: IMatchContent[];
 }
