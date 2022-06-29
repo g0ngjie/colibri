dbd5c213dbacb91a418219317b06d96302fbe1d6
Gj <gongjie0422@63.com>
2022-06-29 23:03:41 +0800

    fix: modify fetch

--- packages/colibri-lib/src/createFetch.ts
+++ packages/colibri-lib/src/createFetch.ts
@@ -11,54 +11,59 @@ export const initFetchState = (state: IRefImpl<IGlobalState>) => globalState = s
 
 function CustomFetch(...args): Promise<Response> {
     let [resource, config] = args;
+    let fetchMethod: string | undefined
     if (config) {
         const reqInit: RequestInit = config
-        const method = reqInit?.method?.toUpperCase()
+        fetchMethod = reqInit?.method?.toUpperCase()
     }
     return OriginFetch(resource, config).then((response: Response) => {
         let txt;
         globalState.value.matching_content.forEach(target => {
-            const { switch_on = true, match_url, override = "", filter_type } = target
+            const { switch_on = true, match_url, override = "", filter_type, method } = target
             // 是否需要匹配
             if (switch_on && match_url) {
+                // 判断是否存在协议匹配
+                if (method && method.toUpperCase() !== fetchMethod) return
                 // 规则匹配
                 const matched = maybeMatching(response.url, match_url, filter_type);
-                if (matched) {
-                    notice(response.url, match_url)
-                    txt = typeof override === "string" ? override : JSON.stringify(override);
-                }
+                if (!matched) return // 退出当前循环
+                // 修改响应
+                txt = typeof override === "string" ? override : JSON.stringify(override);
+                // 通知
+                notice(response.url, match_url)
             }
         });
 
-        if (txt && globalState.value.switch_on) {
-            const stream = new ReadableStream({
-                start(controller) {
-                    controller.enqueue(new TextEncoder().encode(txt));
-                    controller.close();
-                },
-            });
-            const newResponse = new Response(stream, {
-                headers: response.headers,
-                status: response.status,
-                statusText: response.statusText,
-            });
-            const proxy = new Proxy(newResponse, {
-                get: function (target, prop) {
-                    const checkKeys = ['ok', 'redirected', 'type', 'url', 'useFinalURL', 'body', 'bodyUsed'];
-                    if (checkKeys.includes(prop as string)) {
-                        return response[prop];
-                    }
-                    return target[prop];
-                },
-            });
+        // 返回原始响应
+        if (!globalState.value.switch_on || !txt) return response
 
-            for (let key in proxy) {
-                if (typeof proxy[key] === "function") {
-                    proxy[key] = proxy[key].bind(newResponse);
+        const stream = new ReadableStream({
+            start(controller) {
+                controller.enqueue(new TextEncoder().encode(txt));
+                controller.close();
+            },
+        });
+        const newResponse = new Response(stream, {
+            headers: response.headers,
+            status: response.status,
+            statusText: response.statusText,
+        });
+        const proxy = new Proxy(newResponse, {
+            get: function (target, prop) {
+                const checkKeys = ['ok', 'redirected', 'type', 'url', 'useFinalURL', 'body', 'bodyUsed'];
+                if (checkKeys.includes(prop as string)) {
+                    return response[prop];
                 }
+                return target[prop];
+            },
+        });
+
+        for (let key in proxy) {
+            if (typeof proxy[key] === "function") {
+                proxy[key] = proxy[key].bind(newResponse);
             }
-            return proxy;
-        } else return response;
+        }
+        return proxy;
     });
 }
 
