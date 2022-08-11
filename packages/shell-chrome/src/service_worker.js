c3e49205f5828be5608e59a63848a6b1ab964a10
Gj <gongjie0422@163.com>
2022-08-11 11:32:31 +0800

    feat: service_worker

---
+++ packages/shell-chrome/src/service_worker.js
@@ -0,0 +1,6 @@
+
+console.log("Colibri service_worker.js")
+
+chrome.action.setBadgeBackgroundColor({ color: "#006d75" });
+// chrome.action.setBadgeBackgroundColor({ color: "#F56C6C" });
+chrome.action.setBadgeText({ text: '+1' });
