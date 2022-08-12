df092e59e95ed2902cb983325ff237451bf77a07
Gj <gongjie0422@163.com>
2022-08-12 14:11:59 +0800

    fix: modify content

--- packages/shell-chrome/src/content.js
+++ packages/shell-chrome/src/content.js
@@ -21,7 +21,6 @@ initStorage().then(() => {
     const storage = getStorage("sync", "default value is null");
     console.log("[debug]storage:", storage)
     script.addEventListener("load", () => {
-        // ["globalSwitchOn", "proxy_routes", "mode", "redirect"]
         if (getStorage(StorageKey.GLOBAL_SWITCH, false)) {
             noticeDocumentByContent(NoticeKey.GLOBAL_SWITCH, true);
             noticeBackgroundByContent(NoticeKey.GLOBAL_SWITCH, true);
@@ -39,44 +38,12 @@ initStorage().then(() => {
             if (msg.key === NoticeKey.GLOBAL_SWITCH) {
                 noticeDocumentByContent(NoticeKey.GLOBAL_SWITCH, msg.value);
             }
+            // 同步拦截列表
+            if (msg.key === NoticeKey.INTERCEPT_LIST) {
+                noticeDocumentByContent(NoticeKey.INTERCEPT_LIST, msg.value);
+            }
         }
     })
-
-    // 接收background.js传来的信息，转发给core
-    // chrome.runtime.onMessage.addListener((msg) => {
-    //     if (msg.type === "colibri_message" && msg.to === "content") {
-    //         const _isInclude = [
-    //             "globalSwitchOn",
-    //             "proxy_routes",
-    //             "mode",
-    //             "redirect",
-    //         ].includes(msg.key);
-    //         if (_isInclude)
-    //             postMessage({
-    //                 type: "colibri_message",
-    //                 to: "document",
-    //                 key: msg.key,
-    //                 value: msg.value,
-    //             });
-    //     }
-    // });
-
-    // // 接收core传来的信息,转发给background
-    // window.addEventListener(
-    //     "core_notice",
-    //     function (event) {
-    //         if (chrome.runtime) {
-    //             // 转发给background
-    //             chrome.runtime.sendMessage({
-    //                 type: "colibri_message",
-    //                 to: "background",
-    //                 key: "badge",
-    //                 ...event.detail,
-    //             });
-    //         }
-    //     },
-    //     false
-    // );
 
 })
 
