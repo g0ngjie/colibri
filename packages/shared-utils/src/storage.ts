0e2d6acf500f447cdcc269648913537d1c7662ee
Gj <gongjie0422@163.com>
2022-08-12 14:16:24 +0800

    fix: storage sync -> local

--- packages/shared-utils/src/storage.ts
+++ packages/shared-utils/src/storage.ts
@@ -1,8 +1,7 @@
 import { target } from './env'
 
-// If we can, we use the browser extension API to store data
-// it's async though, so we synchronize changes from an intermediate
-// storageData object
+// chrome.storage.sync.set 大于8,192字节的数据时，会报错 -> QUOTA_BYTES_PER_ITEM quota exceeded
+// chrome.storage.local.set可以包含5242880
 const useStorage = typeof target.chrome !== 'undefined' && typeof target.chrome.storage !== 'undefined'
 
 let storageData
@@ -10,7 +9,7 @@ let storageData
 export function initStorage(): Promise<void> {
   return new Promise((resolve) => {
     if (useStorage) {
-      target.chrome.storage.sync.get(null, result => {
+      target.chrome.storage.local.get(null, result => {
         storageData = result
         resolve()
       })
@@ -36,7 +35,7 @@ export function setStorage(key: string, val: any) {
   checkStorage()
   if (useStorage) {
     storageData[key] = val
-    target.chrome.storage.sync.set({ [key]: val })
+    target.chrome.storage.local.set({ [key]: val })
   } else {
     try {
       localStorage.setItem(key, JSON.stringify(val))
@@ -48,7 +47,7 @@ export function removeStorage(key: string) {
   checkStorage()
   if (useStorage) {
     delete storageData[key]
-    target.chrome.storage.sync.remove([key])
+    target.chrome.storage.local.remove([key])
   } else {
     try {
       localStorage.removeItem(key)
@@ -60,7 +59,7 @@ export function clearStorage() {
   checkStorage()
   if (useStorage) {
     storageData = {}
-    target.chrome.storage.sync.clear()
+    target.chrome.storage.local.clear()
   } else {
     try {
       localStorage.clear()
