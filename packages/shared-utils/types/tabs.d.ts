40aca24ec7709fb6639b230be4723fc707190b15
Gj <gongjie0422@163.com>
2022-08-14 09:21:48 +0800

    fix: types

--- packages/shared-utils/types/tabs.d.ts
+++ packages/shared-utils/types/tabs.d.ts
@@ -1,7 +1,26 @@
-/**获取当前标签页 */
-export declare function useCurrentTab(): Promise<{
-    tab: any;
-    url: string | undefined;
-    title: string | undefined;
+interface IChromeTab {
+    active: boolean;
+    audible: boolean;
+    autoDiscardable: boolean;
+    discarded: boolean;
+    favIconUrl: string;
+    groupId: number;
+    height: number;
     highlighted: boolean;
-} | void>;
+    id: number;
+    incognito: boolean;
+    index: number;
+    mutedInfo: {
+        muted: boolean;
+    };
+    pinned: boolean;
+    selected: boolean;
+    status: string;
+    title: string | undefined;
+    url: string | undefined;
+    width: number;
+    windowId: number;
+}
+/**获取当前标签页 */
+export declare function useCurrentTab(): Promise<IChromeTab | void>;
+export {};
